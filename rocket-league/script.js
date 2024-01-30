8d2bce1abcc03561481e697731ee8c3d67e7bd35
Lane17027 <lanenichols17027@gmail.com>
2024-01-30 09:46:26 -0700

    split stats and rank into 2 different grid divs, with a different amount of columns. Added colors based on rank/mmr

--- rocket-league/script.js
+++ rocket-league/script.js
@@ -5,29 +5,42 @@ let statRank = document.getElementById("statRank");
 let statType = document.getElementById("statType");
 let userName = document.getElementById("userName");
 
-
 //Div's for stats
 let statName = document.getElementById("stat-name");
 let statValue = document.getElementById("stat-value");
 
 //Divs for Ranks titles
-let duelTitleDiv=document.getElementById('duel-title')
-let doublesTitleDiv=document.getElementById('doubles-title')
-let standardTitleDiv=document.getElementById('standard-title')
-let hoopsTitleDiv=document.getElementById('hoops-title')
-let rumbleTitleDiv=document.getElementById('rumble-title')
-let dropshotTitleDiv=document.getElementById('dropshot-title')
-let snowDayTitleDiv=document.getElementById('snow-day-title')
-
+let duelTitleDiv = document.getElementById("duel-title");
+let doublesTitleDiv = document.getElementById("doubles-title");
+let standardTitleDiv = document.getElementById("standard-title");
+let hoopsTitleDiv = document.getElementById("hoops-title");
+let rumbleTitleDiv = document.getElementById("rumble-title");
+let dropshotTitleDiv = document.getElementById("dropshot-title");
+let snowDayTitleDiv = document.getElementById("snow-day-title");
 
 //Divs for Rank Values
-let duelValueDiv=document.getElementById('duel-value')
-let doublesValueDiv=document.getElementById('doubles-value')
-let standardValueDiv=document.getElementById('standard-value')
-let hoopsValueDiv=document.getElementById('hoops-value')
-let rumbleValueDiv=document.getElementById('rumble-value')
-let dropshotValueDiv=document.getElementById('dropshot-value')
-let snowDayValueDiv=document.getElementById('snow-day-value')
+let duelValueDiv = document.getElementById("duel-value");
+let doublesValueDiv = document.getElementById("doubles-value");
+let standardValueDiv = document.getElementById("standard-value");
+let hoopsValueDiv = document.getElementById("hoops-value");
+let rumbleValueDiv = document.getElementById("rumble-value");
+let dropshotValueDiv = document.getElementById("dropshot-value");
+let snowDayValueDiv = document.getElementById("snow-day-value");
+
+//Divs for Rank mmr
+let duelMMRDiv = document.getElementById("duel-mmr");
+let doublesMMRDiv = document.getElementById("doubles-mmr");
+let standardMMRDiv = document.getElementById("standard-mmr");
+let hoopsMMRDiv = document.getElementById("hoops-mmr");
+let rumbleMMRDiv = document.getElementById("rumble-mmr");
+let dropshotMMRDiv = document.getElementById("dropshot-mmr");
+let snowDayMMRDiv = document.getElementById("snow-day-mmr");
+
+//All divs that display specific rank
+const allRankDivs = document.querySelectorAll(".rank-value");
+
+//All divs that display MMR
+const allMMRDivs = document.querySelectorAll(".mmr");
 
 const options = {
   method: "GET",
@@ -76,36 +89,87 @@ submitButton.addEventListener("click", async (event) => {
   }
 
   if (statPrefix == "ranks") {
-    options.url=`https://rocket-league1.p.rapidapi.com/ranks/${userNameValue}`
+    options.url = `https://rocket-league1.p.rapidapi.com/ranks/${userNameValue}`;
     let response = await axios.request(options);
     let ranksArr = response.data.ranks;
-    console.log(ranksArr);
     let duel = ranksArr[0];
-    let duelInfo = `${duel.rank}(Division ${duel.division})  MMR: ${duel.mmr}`;
+    let duelInfo = `${duel.rank}(Division ${duel.division})`;
     duelTitleDiv.innerHTML = duel.playlist + ":";
     duelValueDiv.innerHTML = duelInfo;
+    duelMMRDiv.innerHTML = `MMR: ${duel.mmr}`;
 
-    let doubles=ranksArr[1]
-    let doublesInfo=`${doubles.rank}(Division ${doubles.division})  MMR: ${doubles.mmr}`;
-    doublesTitleDiv.innerHTML=doubles.playlist+':'
+    let doubles = ranksArr[1];
+    let doublesInfo = `${doubles.rank}(Division ${doubles.division})`;
+    doublesTitleDiv.innerHTML = doubles.playlist + ":";
     doublesValueDiv.innerHTML = doublesInfo;
-  }
-  // if(statPrefix=='ranks'){
-  //   statType.classList.toggle('remove')
-  // }
-  // let url = `https://rocket-league1.p.rapidapi.com/${statPrefix}/${userNameValue}/${stat}`;
-  // console.log(url)
-  // let response = await axios.get(url, { headers });
-  // console.log(response)
-  // statName.innerHTML = response.data.name + ":";
-  // statValue.innerHTML = response.data.value;
-  // userName.value= "";
-  // statRank.value = "";
-  // statType.value = "";
-});
+    doublesMMRDiv.innerHTML = `MMR: ${doubles.mmr}`;
 
-//for ranks
-// let response=await axios.request(options)
+    let standard = ranksArr[2];
+    let standardInfo = `${standard.rank}(Division ${standard.division})`;
+    standardTitleDiv.innerHTML = standard.playlist + ":";
+    standardValueDiv.innerHTML = standardInfo;
+    standardMMRDiv.innerHTML = `MMR: ${standard.mmr}`;
+
+    // let hoops = ranksArr[3];
+    // let hoopsInfo = `${hoops.rank}(Division ${hoops.division})`;
+    // hoopsTitleDiv.innerHTML = hoops.playlist + ":";
+    // hoopsValueDiv.innerHTML = hoopsInfo;
+    // hoopsMMRDiv.innerHTML = `MMR: ${hoops.mmr}`;
+
+    // let rumble = ranksArr[4];
+    // let rumbleInfo = `${rumble.rank}(Division ${rumble.division})`;
+    // rumbleTitleDiv.innerHTML = rumble.playlist + ":";
+    // rumbleValueDiv.innerHTML = rumbleInfo;
+    // rumbleMMRDiv.innerHTML = `MMR: ${rumble.mmr}`;
+
+    // let dropshot = ranksArr[5];
+    // let dropshotInfo = `${dropshot.rank}(Division ${dropshot.division})`;
+    // dropshotTitleDiv.innerHTML = dropshot.playlist + ":";
+    // dropshotValueDiv.innerHTML = dropshotInfo;
+    // dropshotMMRDiv.innerHTML = `MMR: ${dropshot.mmr}`;
+
+    // let snowDay = ranksArr[3];
+    // let snowDayInfo = `${snowDay.rank}(Division ${snowDay.division})`;
+    // snowDayTitleDiv.innerHTML = snowDay.playlist + ":";
+    // snowDayValueDiv.innerHTML = snowDayInfo;
+    // snowDayMMRDiv.innerHTML = `MMR: ${snowDay.mmr}`;
+
+
+
+
+    for (let rank of allRankDivs) {
+      if (rank.innerHTML.includes("Champion")) {
+        rank.style.color = "purple";
+      } if (rank.innerHTML.includes("Grand")) {
+        rank.style.color = "red";
+      } else if (rank.innerHTML.includes("Gold")) {
+        rank.style.color = "gold";
+      } else if (rank.innerHTML.includes("Silver")) {
+        rank.style.color = "silver";
+      } else if (rank.innerHTML.includes("Bronze")) {
+        rank.style.color = "brown";
+      } else if (rank.innerHTML.includes("Diamond")) {
+        rank.style.color = "#0b07f2";
+      } else if (rank.innerHTML.includes("Platinum")) {
+        rank.style.color = "#1f8ded";
+      }
+    }
+
+    for (let mmr of allMMRDivs){
+      let arr=mmr.innerHTML.split(' ')
+      let mmrNumber=arr[1]
+      if(mmrNumber>=815){
+        mmr.style.color='green'
+
+      }
+      else mmr.style.color='red'
+      // if(mmr.mmr>=815){
+      //   mmr.style.color='green'
+      // }
+      // else mmr.style.color='red'
+    }
+  }
+});
 
 resetButton.addEventListener("click", () => {
   userName.value = "";
@@ -113,4 +177,13 @@ resetButton.addEventListener("click", () => {
   statType.value = "";
   statName.innerHTML = "";
   statValue.innerHTML = "";
+  duelTitleDiv.innerHTML = "";
+  duelValueDiv.innerHTML = "";
+  doublesTitleDiv.innerHTML = "";
+  doublesValueDiv.innerHTML = "";
+  standardTitleDiv.innerHTML = "";
+  standardValueDiv.innerHTML = "";
+  duelMMRDiv.innerHTML = ''
+  doublesMMRDiv.innerHTML = ''
+  standardMMRDiv.innerHTML = ''
 });
