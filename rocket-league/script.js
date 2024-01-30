535ff61a36b04c013f007beed8eea978d5e47aca
Lane17027 <lanenichols17027@gmail.com>
2024-01-30 11:56:14 -0700

    adding all the ranks images

--- rocket-league/script.js
+++ rocket-league/script.js
@@ -36,11 +36,64 @@ let rumbleMMRDiv = document.getElementById("rumble-mmr");
 let dropshotMMRDiv = document.getElementById("dropshot-mmr");
 let snowDayMMRDiv = document.getElementById("snow-day-mmr");
 
+//Divs for images
+let duelImg = document.getElementById("duel-img");
+let doublesImg = document.getElementById("doubles-img");
+let standardImg = document.getElementById("standard-img");
+
 //All divs that display specific rank
 const allRankDivs = document.querySelectorAll(".rank-value");
 
 //All divs that display MMR
 const allMMRDivs = document.querySelectorAll(".mmr");
+
+//Images for every rank
+const unranked =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-0.png'>";
+const bronze1 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-1.png'>";
+const bronze2 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-2.png'>";
+const bronze3 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-3.png'>";
+const silver1 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-4.png'>";
+const silver2 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-5.png'>";
+const silver3 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-6.png'>";
+const gold1 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-7.png'>";
+const gold2 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-8.png'>";
+const gold3 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-9.png'>";
+const platinum1 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-10.png'>";
+const platinum2 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-11.png'>";
+const platinum3 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-12.png'>";
+const diamond1 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-13.png'>";
+const diamond2 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-14.png'>";
+const diamond3 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-15.png'>";
+const champ1 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-16.png'>";
+const champ2 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-17.png'>";
+const champ3 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s4-19.png'>";
+const grandChamp1 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s15rank19.png'>";
+const grandChamp2 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s15rank20.png'>";
+const grandChamp3 =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s15rank21.png'>";
+const superSonicLegend =
+  "<img src='https://trackercdn.com/cdn/tracker.gg/rocket-league/ranks/s15rank22.png'>";
 
 const options = {
   method: "GET",
@@ -134,13 +187,11 @@ submitButton.addEventListener("click", async (event) => {
     // snowDayValueDiv.innerHTML = snowDayInfo;
     // snowDayMMRDiv.innerHTML = `MMR: ${snowDay.mmr}`;
 
-
-
-
     for (let rank of allRankDivs) {
       if (rank.innerHTML.includes("Champion")) {
         rank.style.color = "purple";
-      } if (rank.innerHTML.includes("Grand")) {
+      }
+      if (rank.innerHTML.includes("Grand")) {
         rank.style.color = "red";
       } else if (rank.innerHTML.includes("Gold")) {
         rank.style.color = "gold";
@@ -155,19 +206,228 @@ submitButton.addEventListener("click", async (event) => {
       }
     }
 
-    for (let mmr of allMMRDivs){
-      let arr=mmr.innerHTML.split(' ')
-      let mmrNumber=arr[1]
-      if(mmrNumber>=815){
-        mmr.style.color='green'
-
-      }
-      else mmr.style.color='red'
+    for (let mmr of allMMRDivs) {
+      let arr = mmr.innerHTML.split(" ");
+      let mmrNumber = arr[1];
+      if (mmrNumber >= 815) {
+        mmr.style.color = "green";
+      } else mmr.style.color = "red";
       // if(mmr.mmr>=815){
       //   mmr.style.color='green'
       // }
       // else mmr.style.color='red'
     }
+  }
+
+  let duelRank = duelValueDiv.innerHTML.split("(")[0];
+
+  //Adding Duel Rank Image
+  if (duelValueDiv.innerHTML == "Unranked") {
+    duelImg.innerHTML = unranked;
+  }
+  if (duelRank == "Bronze I") {
+    duelImg.innerHTML = bronze1;
+  }
+  if (duelRank == "Bronze II") {
+    duelImg.innerHTML = bronze2;
+  }
+  if (duelRank == "Bronze III") {
+    duelImg.innerHTML = bronze3;
+  }
+  if (duelRank == "Silver I") {
+    duelImg.innerHTML = silver1;
+  }
+  if (duelRank == "Silver II") {
+    duelImg.innerHTML = silver2;
+  }
+  if (duelRank == "Silver III") {
+    duelImg.innerHTML = silver3;
+  }
+  if (duelRank == "Gold I") {
+    duelImg.innerHTML = gold1;
+  }
+  if (duelRank == "Gold II") {
+    duelImg.innerHTML = gold2;
+  }
+  if (duelRank == "Gold III") {
+    duelImg.innerHTML = gold3;
+  }
+  if (duelRank == "Platinum I") {
+    duelImg.innerHTML = platinum1;
+  }
+  if (duelRank == "Platinum II") {
+    duelImg.innerHTML = platinum2;
+  }
+  if (duelRank == "Platinum III") {
+    duelImg.innerHTML = platinum3;
+  }
+  if (duelRank == "Diamond I") {
+    duelImg.innerHTML = diamond1;
+  }
+  if (duelRank == "Diamond II") {
+    duelImg.innerHTML = diamond2;
+  }
+  if (duelRank == "Diamond III") {
+    duelImg.innerHTML = diamond3;
+  }
+  if (duelRank == "Champion I") {
+    duelImg.innerHTML = champ1;
+  }
+  if (duelRank == "Champion II") {
+    duelImg.innerHTML = champ2;
+  }
+  if (duelRank == "Champion III") {
+    duelImg.innerHTML = champ3;
+  }
+  if (duelRank == "Grand Champion I") {
+    duelImg.innerHTML = grandChamp1;
+  }
+  if (duelRank == "Grand Champion II") {
+    duelImg.innerHTML = grandChamp2;
+  }
+  if (duelRank == "Grand Champion III") {
+    duelImg.innerHTML = grandChamp3;
+  }
+
+  //Adding doubles Img
+
+  let doublesRank = doublesValueDiv.innerHTML.split("(")[0];
+
+  if (doublesValueDiv.innerHTML == "Unranked") {
+    doublesImg.innerHTML = unranked;
+  }
+  if (doublesRank == "Bronze I") {
+    doublesImg.innerHTML = bronze1;
+  }
+  if (doublesRank == "Bronze II") {
+    doublesImg.innerHTML = bronze2;
+  }
+  if (doublesRank == "Bronze III") {
+    doublesImg.innerHTML = bronze3;
+  }
+  if (doublesRank == "Silver I") {
+    doublesImg.innerHTML = silver1;
+  }
+  if (doublesRank == "Silver II") {
+    doublesImg.innerHTML = silver2;
+  }
+  if (doublesRank == "Silver III") {
+    doublesImg.innerHTML = silver3;
+  }
+  if (doublesRank == "Gold I") {
+    doublesImg.innerHTML = gold1;
+  }
+  if (doublesRank == "Gold II") {
+    doublesImg.innerHTML = gold2;
+  }
+  if (doublesRank == "Gold III") {
+    doublesImg.innerHTML = gold3;
+  }
+  if (doublesRank == "Platinum I") {
+    doublesImg.innerHTML = platinum1;
+  }
+  if (doublesRank == "Platinum II") {
+    doublesImg.innerHTML = platinum2;
+  }
+  if (doublesRank == "Platinum III") {
+    doublesImg.innerHTML = platinum3;
+  }
+  if (doublesRank == "Diamond I") {
+    doublesImg.innerHTML = diamond1;
+  }
+  if (doublesRank == "Diamond II") {
+    doublesImg.innerHTML = diamond2;
+  }
+  if (doublesRank == "Diamond III") {
+    doublesImg.innerHTML = diamond3;
+  }
+  if (doublesRank == "Champion I") {
+    doublesImg.innerHTML = champ1;
+  }
+  if (doublesRank == "Champion II") {
+    doublesImg.innerHTML = champ2;
+  }
+  if (doublesRank == "Champion III") {
+    doublesImg.innerHTML = champ3;
+  }
+  if (doublesRank == "Grand Champion I") {
+    doublesImg.innerHTML = grandChamp1;
+  }
+  if (doublesRank == "Grand Champion II") {
+    doublesImg.innerHTML = grandChamp2;
+  }
+  if (doublesRank == "Grand Champion III") {
+    doublesImg.innerHTML = grandChamp3;
+  }
+
+  //Adding standard Img
+  let standardRank = standardValueDiv.innerHTML.split("(")[0];
+
+  if (standardValueDiv.innerHTML == "Unranked") {
+    standardImg.innerHTML = unranked;
+  }
+  if (standardRank == "Bronze I") {
+    standardImg.innerHTML = bronze1;
+  }
+  if (standardRank == "Bronze II") {
+    standardImg.innerHTML = bronze2;
+  }
+  if (standardRank == "Bronze III") {
+    standardImg.innerHTML = bronze3;
+  }
+  if (standardRank == "Silver I") {
+    standardImg.innerHTML = silver1;
+  }
+  if (standardRank == "Silver II") {
+    standardImg.innerHTML = silver2;
+  }
+  if (standardRank == "Silver III") {
+    standardImg.innerHTML = silver3;
+  }
+  if (standardRank == "Gold I") {
+    standardImg.innerHTML = gold1;
+  }
+  if (standardRank == "Gold II") {
+    standardImg.innerHTML = gold2;
+  }
+  if (standardRank == "Gold III") {
+    standardImg.innerHTML = gold3;
+  }
+  if (standardRank == "Platinum I") {
+    standardImg.innerHTML = platinum1;
+  }
+  if (standardRank == "Platinum II") {
+    standardImg.innerHTML = platinum2;
+  }
+  if (standardRank == "Platinum III") {
+    standardImg.innerHTML = platinum3;
+  }
+  if (standardRank == "Diamond I") {
+    standardImg.innerHTML = diamond1;
+  }
+  if (standardRank == "Diamond II") {
+    standardImg.innerHTML = diamond2;
+  }
+  if (standardRank == "Diamond III") {
+    standardImg.innerHTML = diamond3;
+  }
+  if (standardRank == "Champion I") {
+    standardImg.innerHTML = champ1;
+  }
+  if (standardRank == "Champion II") {
+    standardImg.innerHTML = champ2;
+  }
+  if (standardRank == "Champion III") {
+    standardImg.innerHTML = champ3;
+  }
+  if (standardRank == "Grand Champion I") {
+    standardImg.innerHTML = grandChamp1;
+  }
+  if (standardRank == "Grand Champion II") {
+    standardImg.innerHTML = grandChamp2;
+  }
+  if (standardRank == "Grand Champion III") {
+    standardImg.innerHTML = grandChamp3;
   }
 });
 
@@ -183,7 +443,10 @@ resetButton.addEventListener("click", () => {
   doublesValueDiv.innerHTML = "";
   standardTitleDiv.innerHTML = "";
   standardValueDiv.innerHTML = "";
-  duelMMRDiv.innerHTML = ''
-  doublesMMRDiv.innerHTML = ''
-  standardMMRDiv.innerHTML = ''
+  duelMMRDiv.innerHTML = "";
+  doublesMMRDiv.innerHTML = "";
+  standardMMRDiv.innerHTML = "";
+  duelImg.innerHTML = "";
+  doublesImg.innerHTML = "";
+  standardImg.innerHTML = "";
 });
