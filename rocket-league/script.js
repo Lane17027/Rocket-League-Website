cd31b6182e9a048039eb79afc576825ecd6b164b
Lane17027 <lanenichols17027@gmail.com>
2024-02-01 11:37:35 -0700

    added the extra game modes data

--- rocket-league/script.js
+++ rocket-league/script.js
@@ -40,12 +40,19 @@ let snowDayMMRDiv = document.getElementById("snow-day-mmr");
 let duelImg = document.getElementById("duel-img");
 let doublesImg = document.getElementById("doubles-img");
 let standardImg = document.getElementById("standard-img");
+let hoopsImg=document.getElementById('hoops-img')
+let rumbleImg=document.getElementById('rumble-img')
+let dropshotImg=document.getElementById('dropshot-img')
+
 
 //All divs that display specific rank
 const allRankDivs = document.querySelectorAll(".rank-value");
 
 //All divs that display MMR
 const allMMRDivs = document.querySelectorAll(".mmr");
+
+//All divs to reset
+const allReset=document.querySelectorAll('.reset')
 
 //Images for every rank
 const unranked =
@@ -136,7 +143,6 @@ submitButton.addEventListener("click", async (event) => {
   if (statPrefix == "stat") {
     let url = `https://rocket-league1.p.rapidapi.com/${statPrefix}/${userNameValue}/${stat}`;
     let response = await axios.get(url, { headers });
-    console.log(url);
     statName.innerHTML = response.data.name + ":";
     statValue.innerHTML = response.data.value;
   }
@@ -144,7 +150,6 @@ submitButton.addEventListener("click", async (event) => {
   if (statPrefix == "ranks") {
     options.url = `https://rocket-league1.p.rapidapi.com/ranks/${userNameValue}`;
     let response = await axios.request(options);
-    console.log(response)
     let ranksArr = response.data.ranks;
     let duel = ranksArr[0];
     let duelInfo = `${duel.rank}(Division ${duel.division})`;
@@ -164,31 +169,32 @@ submitButton.addEventListener("click", async (event) => {
     standardValueDiv.innerHTML = standardInfo;
     standardMMRDiv.innerHTML = `MMR: ${standard.mmr}`;
 
-    // let hoops = ranksArr[3];
-    // let hoopsInfo = `${hoops.rank}(Division ${hoops.division})`;
-    // hoopsTitleDiv.innerHTML = hoops.playlist + ":";
-    // hoopsValueDiv.innerHTML = hoopsInfo;
-    // hoopsMMRDiv.innerHTML = `MMR: ${hoops.mmr}`;
+    let hoops = ranksArr[3];
+    let hoopsInfo = `${hoops.rank}(Division ${hoops.division})`;
+    hoopsTitleDiv.innerHTML = hoops.playlist + ":";
+    hoopsValueDiv.innerHTML = hoopsInfo;
+    hoopsMMRDiv.innerHTML = `MMR: ${hoops.mmr}`;
 
-    // let rumble = ranksArr[4];
-    // let rumbleInfo = `${rumble.rank}(Division ${rumble.division})`;
-    // rumbleTitleDiv.innerHTML = rumble.playlist + ":";
-    // rumbleValueDiv.innerHTML = rumbleInfo;
-    // rumbleMMRDiv.innerHTML = `MMR: ${rumble.mmr}`;
+    let rumble = ranksArr[4];
+    let rumbleInfo = `${rumble.rank}(Division ${rumble.division})`;
+    rumbleTitleDiv.innerHTML = rumble.playlist + ":";
+    rumbleValueDiv.innerHTML = rumbleInfo;
+    rumbleMMRDiv.innerHTML = `MMR: ${rumble.mmr}`;
 
-    // let dropshot = ranksArr[5];
-    // let dropshotInfo = `${dropshot.rank}(Division ${dropshot.division})`;
-    // dropshotTitleDiv.innerHTML = dropshot.playlist + ":";
-    // dropshotValueDiv.innerHTML = dropshotInfo;
-    // dropshotMMRDiv.innerHTML = `MMR: ${dropshot.mmr}`;
+    let dropshot = ranksArr[5];
+    let dropshotInfo = `${dropshot.rank}(Division ${dropshot.division})`;
+    dropshotTitleDiv.innerHTML = dropshot.playlist + ":";
+    dropshotValueDiv.innerHTML = dropshotInfo;
+    dropshotMMRDiv.innerHTML = `MMR: ${dropshot.mmr}`;
 
-    // let snowDay = ranksArr[3];
-    // let snowDayInfo = `${snowDay.rank}(Division ${snowDay.division})`;
-    // snowDayTitleDiv.innerHTML = snowDay.playlist + ":";
-    // snowDayValueDiv.innerHTML = snowDayInfo;
-    // snowDayMMRDiv.innerHTML = `MMR: ${snowDay.mmr}`;
+    let snowDay = ranksArr[3];
+    let snowDayInfo = `${snowDay.rank}(Division ${snowDay.division})`;
+    snowDayTitleDiv.innerHTML = snowDay.playlist + ":";
+    snowDayValueDiv.innerHTML = snowDayInfo;
+    snowDayMMRDiv.innerHTML = `MMR: ${snowDay.mmr}`;
 
     for (let rank of allRankDivs) {
+      console.log(rank);
       if (rank.innerHTML.includes("Champion")) {
         rank.style.color = "purple";
       }
@@ -204,6 +210,8 @@ submitButton.addEventListener("click", async (event) => {
         rank.style.color = "#0b07f2";
       } else if (rank.innerHTML.includes("Platinum")) {
         rank.style.color = "#00d4ff";
+      } else if (rank.innerHTML.includes("Unranked")) {
+        rank.style.color = "gray";
       }
     }
 
@@ -213,10 +221,6 @@ submitButton.addEventListener("click", async (event) => {
       if (mmrNumber >= 815) {
         mmr.style.color = "green";
       } else mmr.style.color = "red";
-      // if(mmr.mmr>=815){
-      //   mmr.style.color='green'
-      // }
-      // else mmr.style.color='red'
     }
   }
 
@@ -430,24 +434,97 @@ submitButton.addEventListener("click", async (event) => {
   if (standardRank == "Grand Champion III") {
     standardImg.innerHTML = grandChamp3;
   }
+
+//adding  hoops div image
+  let hoopsRank = hoopsValueDiv.innerHTML.split("(")[0]
+
+  if (hoopsValueDiv.innerHTML == "Unranked") {
+    hoopsImg.innerHTML = unranked;
+  }
+  if (hoopsRank == "Bronze I") {
+    hoopsImg.innerHTML = bronze1;
+  }
+  if (hoopsRank == "Bronze II") {
+    hoopsImg.innerHTML = bronze2;
+  }
+  if (hoopsRank == "Bronze III") {
+    hoopsImg.innerHTML = bronze3;
+  }
+  if (hoopsRank == "Silver I") {
+    hoopsImg.innerHTML = silver1;
+  }
+  if (hoopsRank == "Silver II") {
+    hoopsImg.innerHTML = silver2;
+  }
+  if (hoopsRank == "Silver III") {
+    hoopsImg.innerHTML = silver3;
+  }
+  if (hoopsRank == "Gold I") {
+    hoopsImg.innerHTML = gold1;
+  }
+  if (hoopsRank == "Gold II") {
+    hoopsImg.innerHTML = gold2;
+  }
+  if (hoopsRank == "Gold III") {
+    hoopsImg.innerHTML = gold3;
+  }
+  if (hoopsRank == "Platinum I") {
+    hoopsImg.innerHTML = platinum1;
+  }
+  if (hoopsRank == "Platinum II") {
+    hoopsImg.innerHTML = platinum2;
+  }
+  if (hoopsRank == "Platinum III") {
+    hoopsImg.innerHTML = platinum3;
+  }
+  if (hoopsRank == "Diamond I") {
+    hoopsImg.innerHTML = diamond1;
+  }
+  if (hoopsRank == "Diamond II") {
+    hoopsImg.innerHTML = diamond2;
+  }
+  if (hoopsRank == "Diamond III") {
+    hoopsImg.innerHTML = diamond3;
+  }
+  if (hoopsRank == "Champion I") {
+    hoopsImg.innerHTML = champ1;
+  }
+  if (hoopsRank == "Champion II") {
+    hoopsImg.innerHTML = champ2;
+  }
+  if (hoopsRank == "Champion III") {
+    hoopsImg.innerHTML = champ3;
+  }
+  if (hoopsRank == "Grand Champion I") {
+    hoopsImg.innerHTML = grandChamp1;
+  }
+  if (hoopsRank == "Grand Champion II") {
+    hoopsImg.innerHTML = grandChamp2;
+  }
+  if (hoopsRank == "Grand Champion III") {
+    hoopsImg.innerHTML = grandChamp3;
+  }
 });
 
 resetButton.addEventListener("click", () => {
-  userName.value = "";
-  statRank.value = "";
-  statType.value = "";
-  statName.innerHTML = "";
-  statValue.innerHTML = "";
-  duelTitleDiv.innerHTML = "";
-  duelValueDiv.innerHTML = "";
-  doublesTitleDiv.innerHTML = "";
-  doublesValueDiv.innerHTML = "";
-  standardTitleDiv.innerHTML = "";
-  standardValueDiv.innerHTML = "";
-  duelMMRDiv.innerHTML = "";
-  doublesMMRDiv.innerHTML = "";
-  standardMMRDiv.innerHTML = "";
-  duelImg.innerHTML = "";
-  doublesImg.innerHTML = "";
-  standardImg.innerHTML = "";
+  // userName.value = "";
+  // statRank.value = "";
+  // statType.value = "";
+  // statName.innerHTML = "";
+  // statValue.innerHTML = "";
+  // duelTitleDiv.innerHTML = "";
+  // duelValueDiv.innerHTML = "";
+  // doublesTitleDiv.innerHTML = "";
+  // doublesValueDiv.innerHTML = "";
+  // standardTitleDiv.innerHTML = "";
+  // standardValueDiv.innerHTML = "";
+  // duelMMRDiv.innerHTML = "";
+  // doublesMMRDiv.innerHTML = "";
+  // standardMMRDiv.innerHTML = "";
+  // duelImg.innerHTML = "";
+  // doublesImg.innerHTML = "";
+  // standardImg.innerHTML = "";
+  for (let reset of allReset){
+    reset.innerHTML=''
+  }
 });
